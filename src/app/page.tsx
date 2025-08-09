f5ab2db348843bbf4023eae59c1e69ad01bb5b01
DoubleA <ruben.aadland@gmail.com>
2025-08-09 11:20:15 +0200

    Migrated test page to index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import {Button} from "@/components/ui/button";
+import DashboardHero from "@/components/dashboard/DashboardHero";
+import DayList from "@/components/dashboard/DayList";
+import StatsCarousel from "@/components/dashboard/StatsCarousel";
 
 export default function Home() {
     return (
-        <Button variant={"default"}>
-            Test
-        </Button>
+        <>
+            <DashboardHero variant={"opened"}/>
+            <section className={"grid text-center py-8"}>
+                <span className={"text-7xl"}>200kr</span>
+                {/*TODO - Only show if the last day has had a win*/}
+                <span className={"text-sm text-green-500"}>+50kr</span>
+                <p className={"text-lg"}>Total gevinst</p>
+            </section>
+            <DayList/>
+            <StatsCarousel/>
+        </>
     );
 }
